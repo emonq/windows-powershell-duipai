e1519f642fff35c93a032691b1898c25291a9242
emonq <emonq@outlook.com>
2019-11-20 20:35:40 +0800

    增加总时间计时

--- dp.ps1
+++ dp.ps1
@@ -1,5 +1,13 @@
-$errorcode=0
+$errorcode=0;$totmytime=0;$totstdtime=0;
+[Console]::TreatControlCAsInput = $True;
 while (!$errorcode) {
+    If ($Host.UI.RawUI.KeyAvailable -and ($Key = $Host.UI.RawUI.ReadKey("AllowCtrlC,NoEcho,IncludeKeyUp"))) {
+        If ([Int]$Key.Character -eq 3) {
+            Write-Host "KeyboardInterrupted" -ForegroundColor Red;
+            [Console]::TreatControlCAsInput = $False;
+            break;
+        }
+    }
     ./random | Out-File data.in;
     $sw1 = [Diagnostics.Stopwatch]::StartNew();
     Get-Content data.in | ./my | Out-File my.out;
@@ -11,11 +19,17 @@ while (!$errorcode) {
     $sw2.Stop();
     Write-Host "No difference" -BackgroundColor Green;
     Write-Host ("my.exe finished with exit code "+$my_code+" within "+$sw1.Elapsed.TotalMilliseconds+"ms");
-    Write-Host ("std.exe finished with exit code "+$std_code+" within "+$sw2.Elapsed.TotalMilliseconds+"ms");
+    Write-Host ("std.exe finished with exit code "+$std_code+" within "+$sw2.Elapsed.TotalMilliseconds+"ms"),`n;
+    $totmytime+=$sw1.Elapsed.TotalMilliseconds
+    $totstdtime+=$sw2.Elapsed.TotalMilliseconds
     $my=Get-Content my.out;
     $std=Get-Content std.out;
     $errorcode=Compare-Object $my $std
 }
-Write-Host "Error occurred:","******my.out******",$my,"******std.out******",$std,"******data.in*******",(Get-Content data.in) -Separator "`n"
-Write-Host "******finished******" -BackgroundColor Yellow -ForegroundColor Red 
+if($errorcode){
+    Write-Host "Error occurred:","******my.out******",$my,"******std.out******",$std,"******data.in*******",(Get-Content data.in) -Separator "`n"
+    Write-Host "******finished******" -BackgroundColor Yellow -ForegroundColor Red
+}
+Write-Host ("my.exe used "+$totmytime+" ms") -ForegroundColor Gray
+Write-Host ("std.exe used "+$totstdtime+" ms") -ForegroundColor Gray
 Pause;
