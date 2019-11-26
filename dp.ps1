093811c9a9dcb30b7736453fba06ca84535133ed
emonq <emonq@outlook.com>
2019-11-26 12:13:53 +0800

    fixed

--- dp.ps1
+++ dp.ps1
@@ -9,25 +9,36 @@ while (!$errorcode) {
         }
     }
     ./random | Out-File data.in;
+    if($?){Write-Host "data.in Generated" -ForegroundColor Green}
+    else {
+        Write-Host `r,"data.in Generation failed" -ForegroundColor Red
+        break
+    }
     $sw1 = [Diagnostics.Stopwatch]::StartNew();
     Get-Content data.in | ./my | Out-File my.out;
     $my_code=$LASTEXITCODE;
     $sw1.Stop();
+    if($my_code){
+        Write-Host "my.exe went wrong" -ForegroundColor Red
+        break
+    }
+    Write-Host ("my.exe finished with exit code "+$my_code+" within "+$sw1.Elapsed.TotalMilliseconds+"ms");
     $sw2 = [Diagnostics.Stopwatch]::StartNew();
     Get-Content data.in | ./std | Out-File std.out;
     $std_code=$LASTEXITCODE;
     $sw2.Stop();
-    Write-Host "No difference" -BackgroundColor Green;
-    Write-Host ("my.exe finished with exit code "+$my_code+" within "+$sw1.Elapsed.TotalMilliseconds+"ms");
-    Write-Host ("std.exe finished with exit code "+$std_code+" within "+$sw2.Elapsed.TotalMilliseconds+"ms"),`n;
+    Write-Host ("std.exe finished with exit code "+$std_code+" within "+$sw2.Elapsed.TotalMilliseconds+"ms");
     $totmytime+=$sw1.Elapsed.TotalMilliseconds
     $totstdtime+=$sw2.Elapsed.TotalMilliseconds
     $my=Get-Content my.out;
     $std=Get-Content std.out;
-    $errorcode=Compare-Object $my $std
+    if(!$my -and !$std){$errorcode=0}
+    else {$errorcode=Compare-Object $my $std}
+    if($errorcode){break}
+    else {Write-Host "No difference" -ForegroundColor Green;}
 }
 if($errorcode){
-    Write-Host "Error occurred:","******my.out******",$my,"******std.out******",$std,"******data.in*******",(Get-Content data.in) -Separator "`n"
+    Write-Host "Error occurred:","******my.out******",$my,"******std.out******",$std,"******data.in*******",(Get-Content data.in),`n -Separator "`n"
     Write-Host "******finished******" -BackgroundColor Yellow -ForegroundColor Red
 }
 Write-Host ("my.exe used "+$totmytime+" ms") -ForegroundColor Gray
